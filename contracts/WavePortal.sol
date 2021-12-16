cd07ae375f04a276a5a3da76d0c10748ab237b0b
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-16 11:25:51 -0500

    Store data

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,24 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  //TODO: partnerList partner[]
+  //TODO: partner {name,message,waveNumber}
+  //TODO: name string
+  //TODO: message string
+  //TODO: waveNumber number
+
   constructor(){
     console.log("Smart contracts for babies");
+  }
+
+  function wave() public{
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256){
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
   }
 }
